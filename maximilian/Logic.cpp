130526ff81fa19605791ddb0f60a6109f61d64c1
Максимилиан <max4tim@bk.ru>
2022-07-10 19:39:36 +0300

    Template implementation of action's triada
    SEARCHE - turn around
    DUEL - shut and run toward
    RUNAWAY - shut and run backward

--- maximilian/Logic.cpp
+++ maximilian/Logic.cpp
@@ -1,14 +1,71 @@
 #include "Logic.hpp"
 
-Logic::Logic() : state(State::AGRESSIVE) {
+Logic::Logic() : state(State::SEARCHE) {
 
 }
 
 model::UnitOrder Logic::getOrder(const model::Game& game, DebugInterface* debugInterface, const model::Unit& unit) {
+    model::Vec2 targetVelocity(-unit.position.x, -unit.position.y);
+    model::Vec2 targetDirection(-unit.direction.y, unit.direction.x);
+
+    for (bool decided = false; !decided;) switch (state) {
+    case State::SEARCHE:
+        if (game.units.size() > 1) {
+            state = State::DUEL;
+            break;
+        }
+
+        targetVelocity = model::Vec2(-unit.position.x, -unit.position.y);
+        targetDirection = model::Vec2(-unit.position.x, -unit.position.y);
+        decided = true;
+        break;
+    case State::DUEL:
+        if (game.units.size() > 2) {
+            state = State::RUNAWAY;
+            break;
+        }
+        if (game.units.size() < 2) {
+            state = State::SEARCHE;
+            break;
+        }
+        const model::Unit* enemyPtr;
+        for (const model::Unit& u : game.units) {
+            if (u.id != unit.id) {
+                enemyPtr = &u;
+            }
+        }
+
+        targetVelocity = model::Vec2(enemyPtr->position.x - unit.position.x, enemyPtr->position.y - unit.position.y);
+        targetDirection = model::Vec2(enemyPtr->position.x - unit.position.x, enemyPtr->position.y - unit.position.y);
+
+        decided = true;
+        break;
+    case State::RUNAWAY:
+        vector<const model::Unit*> enemyPtrs;
+        for (const model::Unit& u : game.units) {
+            if (u.id != unit.id) {
+                enemyPtrs.push_back(&u);
+            }
+        }
+
+        model::Vec2 averageEnemyPos(0., 0.);
+        for (const model::Unit* enemyPtr : enemyPtrs) {
+            averageEnemyPos += enemyPtr->position;
+        }
+        averageEnemyPos /= enemyPtrs.size();
+
+        targetVelocity = model::Vec2(-(averageEnemyPos.x - unit.position.x), -(averageEnemyPos.y - unit.position.y));
+        targetDirection = model::Vec2(averageEnemyPos.x - unit.position.x, averageEnemyPos.y - unit.position.y);
+
+        decided = true;
+        break;
+    }
+
+
     std::shared_ptr<model::ActionOrder::Aim> aim = std::make_shared<model::ActionOrder::Aim>(true);
     std::optional<std::shared_ptr<model::ActionOrder>> action = std::make_optional(aim);
 
-    model::UnitOrder order({ -unit.position.x, -unit.position.y }, { -unit.direction.y, unit.direction.x }, action);
+    model::UnitOrder order(targetVelocity, targetDirection, action);
 
     return order;
 }
--- maximilian/Logic.hpp
+++ maximilian/Logic.hpp
@@ -3,10 +3,19 @@
 #include "model/Order.hpp"
 #include "model/Constants.hpp"
 
+#include "Operators.hpp"
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Logic {
 public:
-    enum State {
-        AGRESSIVE = 0,
+    enum State {     // Простая система:
+        SEARCHE = 0, // если врагов нет, ищем
+        DUEL,        // если враг один, сражаемся
+        RUNAWAY      // если врагов >1, бежим
     };
 
     Logic();
@@ -14,4 +23,6 @@ public:
     model::UnitOrder getOrder(const model::Game& game, DebugInterface* debugInterface, const model::Unit& unit);
 private:
     State state;
+
+    int countEnemy(const model::Game& game);
 };
